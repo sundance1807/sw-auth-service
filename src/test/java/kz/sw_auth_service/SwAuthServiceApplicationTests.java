2ac08092b3ff85f3b64bd4c43321004420b95acf
Askar.A <aalyiev7@gmail.com>
2024-06-04 14:09:59 +0500

    created dockerfile and docker-compose

--- src/test/java/kz/sw_auth_service/SwAuthServiceApplicationTests.java
+++ src/test/java/kz/sw_auth_service/SwAuthServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SwAuthServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
